987932ae88df86ab190ecbe4f373cde7501fdfdd
iamMusa <godwinobas0@gmail.com>
2023-08-25 11:47:35 +0100

    Linked signup View to backend

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require ("express");
 const mongoose = require ("mongoose");
 const authRoutes = require('./routes/authRoutes');
+const cookieparser = require('cookie-parser');
 
 const app = express();
 
 // Middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieparser());
 
 // View Engine
 app.set('view engine', 'ejs');
@@ -21,3 +23,5 @@ mongoose.connect(dbURI)
 app.get('/', (req, res) => res.render('home'));
 app.get('/kunu', (req, res) => res.render('kunu'));
 app.use(authRoutes);
+
+//  cookies
