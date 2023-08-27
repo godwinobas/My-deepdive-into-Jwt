ada6c04901dbeb7b39572eccc7e988ec0f93207e
iamMusa <godwinobas0@gmail.com>
2023-08-27 18:51:51 +0100

    added login functionality

--- models/User.js
+++ models/User.js
@@ -13,7 +13,7 @@ const userSchema = new mongoose.Schema({
     password:{
         type: String,
         required: [true, 'Please enter a password'],
-        minlength: [6, 'Minnimum password length is 6 characters']
+        minlength: [6, 'Minimum password length is 6 characters']
     },
 });
 
@@ -23,6 +23,19 @@ userSchema.pre('save', async function (next) {
     this.password = await bcrypt.hash(this.password, salt);
     next();
 });
+
+// Static methd to login user
+userSchema.statics.login = async function(email, password) {
+    const user = await this.findOne({ email });
+    if (user) {
+       const auth = await bcrypt.compare(password, user.password);
+       if (auth) {
+        return user;
+       }
+       throw Error('incorrect password');
+    }
+    throw Error('incorrect email');
+}
 
 const User = mongoose.model('user', userSchema);
 
